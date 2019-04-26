c8d6c599e54bdbb49b541670297089b2f32db7b7
Justin Beckwith <justin.beckwith@gmail.com>
2019-04-25 21:20:35 -0700

    mor

--- test/test.ts
+++ test/test.ts
@@ -31,8 +31,8 @@ const gcxp = proxyquire('../src/index', {
           }
         };
       }
-    }
-  }
+    },
+  },
 });
 
 describe('validation', () => {
@@ -42,27 +42,33 @@ describe('validation', () => {
 
   it('should throw if multiple triggers are provided', () => {
     assert.rejects(
-        gcx.deploy({name, triggerBucket: 'bukkit', triggerTopic: 'toppi'}));
+      gcx.deploy({ name, triggerBucket: 'bukkit', triggerTopic: 'toppi' })
+    );
   });
 });
 
 describe('ignore rules', () => {
   it('should return 0 rules if no .gcloudignore is availabe', async () => {
-    const deployer = new gcx.Deployer({name});
+    const deployer = new gcx.Deployer({ name });
     const rules = await deployer._getIgnoreRules();
     assert.deepStrictEqual(rules, []);
   });
 
   it('should return expected rules if .gcloudignore is availabe', async () => {
-    const expected =
-        ['.gcloudignore', '.git', '.gitignore', 'node_modules', 'test/'];
+    const expected = [
+      '.gcloudignore',
+      '.git',
+      '.gitignore',
+      'node_modules',
+      'test/',
+    ];
     await new Promise((resolve, reject) => {
       fs.createReadStream(gcloudignore)
-          .pipe(fs.createWriteStream('.gcloudignore'))
-          .on('close', resolve)
-          .on('error', reject);
+        .pipe(fs.createWriteStream('.gcloudignore'))
+        .on('close', resolve)
+        .on('error', reject);
     });
-    const deployer = new gcx.Deployer({name});
+    const deployer = new gcx.Deployer({ name });
     const rules = await deployer._getIgnoreRules();
     await unlink('.gcloudignore');
     assert.deepStrictEqual(rules, expected);
@@ -73,9 +79,9 @@ describe('pack & upload', () => {
   let zipFile: string;
 
   it('should pack all of the files in the target dir', async () => {
-    const deployer = new gcx.Deployer({name, targetDir});
+    const deployer = new gcx.Deployer({ name, targetDir });
     zipFile = await deployer._pack();
-    const zip = new Zip({file: zipFile, storeEntries: true});
+    const zip = new Zip({ file: zipFile, storeEntries: true });
     await new Promise((resolve, reject) => {
       zip.on('error', reject).on('ready', () => {
         const files = Object.keys(zip.entries());
@@ -88,7 +94,7 @@ describe('pack & upload', () => {
   });
 
   it('should PUT the file to Google Cloud Storage', async () => {
-    const deployer = new gcx.Deployer({name, targetDir});
+    const deployer = new gcx.Deployer({ name, targetDir });
     const scope = mockUpload();
     await deployer._upload(zipFile, 'https://fake.local');
     scope.done();
@@ -98,7 +104,7 @@ describe('pack & upload', () => {
 describe('cloud functions api', () => {
   it('should check to see if the function exists', async () => {
     const scopes = [mockExists()];
-    const deployer = new gcxp.Deployer({name});
+    const deployer = new gcxp.Deployer({ name });
     const fullName = `projects/el-gato/locations/us-central1/functions/${name}`;
     const exists = await deployer._exists(fullName);
     assert.strictEqual(exists, true);
@@ -110,7 +116,7 @@ describe('end to end', () => {
   it('should deploy end to end', async () => {
     const scopes = [mockUploadUrl(), mockUpload(), mockDeploy(), mockPoll()];
     const projectId = 'el-gato';
-    const deployer = new gcxp.Deployer({name, targetDir, projectId});
+    const deployer = new gcxp.Deployer({ name, targetDir, projectId });
     await deployer.deploy();
     scopes.forEach(s => s.done());
   });
@@ -118,7 +124,7 @@ describe('end to end', () => {
   it('should call end to end', async () => {
     const scopes = [mockCall()];
     const c = new gcxp.Caller();
-    const res = await c.call({functionName: name});
+    const res = await c.call({ functionName: name });
     assert.strictEqual(res.data.result, '{ "data": 42 }');
     scopes.forEach(s => s.done());
   });
@@ -126,7 +132,7 @@ describe('end to end', () => {
   it('should call end to end with data', async () => {
     const scopes = [mockCallWithData()];
     const c = new gcxp.Caller();
-    const res = await c.call({functionName: name, data: 142});
+    const res = await c.call({ functionName: name, data: 142 });
     assert.strictEqual(res.data.result, '{ "data": 142 }');
     scopes.forEach(s => s.done());
   });
@@ -134,38 +140,39 @@ describe('end to end', () => {
 
 function mockUpload() {
   return nock('https://fake.local', {
-           reqheaders: {
-             'Content-Type': 'application/zip',
-             'x-goog-content-length-range': '0,104857600'
-           }
-         })
-      .put('/')
-      .reply(200);
+    reqheaders: {
+      'Content-Type': 'application/zip',
+      'x-goog-content-length-range': '0,104857600',
+    },
+  })
+    .put('/')
+    .reply(200);
 }
 
 function mockUploadUrl() {
   return nock('https://cloudfunctions.googleapis.com')
-      .post(
-          '/v1/projects/el-gato/locations/us-central1/functions:generateUploadUrl')
-      .reply(200, {uploadUrl: 'https://fake.local'});
+    .post(
+      '/v1/projects/el-gato/locations/us-central1/functions:generateUploadUrl'
+    )
+    .reply(200, { uploadUrl: 'https://fake.local' });
 }
 
 function mockDeploy() {
   return nock('https://cloudfunctions.googleapis.com')
-      .post('/v1/projects/el-gato/locations/us-central1/functions')
-      .reply(200, {name: 'not-a-real-operation'});
+    .post('/v1/projects/el-gato/locations/us-central1/functions')
+    .reply(200, { name: 'not-a-real-operation' });
 }
 
 function mockPoll() {
   return nock('https://cloudfunctions.googleapis.com')
-      .get('/v1/not-a-real-operation')
-      .reply(200, {done: true});
+    .get('/v1/not-a-real-operation')
+    .reply(200, { done: true });
 }
 
 function mockExists() {
   return nock('https://cloudfunctions.googleapis.com')
-      .get('/v1/projects/el-gato/locations/us-central1/functions/%F0%9F%A6%84')
-      .reply(200);
+    .get('/v1/projects/el-gato/locations/us-central1/functions/%F0%9F%A6%84')
+    .reply(200);
 }
 
 /**
@@ -173,13 +180,14 @@ function mockExists() {
  */
 function mockCall() {
   return nock('https://cloudfunctions.googleapis.com')
-      .post(
-          '/v1/projects/el-gato/locations/us-central1/function/%F0%9F%A6%84:call')
-      .reply(200, {
-        executionId: 'my-execution-id',
-        result: '{ "data": 42 }',
-        error: null
-      });
+    .post(
+      '/v1/projects/el-gato/locations/us-central1/function/%F0%9F%A6%84:call'
+    )
+    .reply(200, {
+      executionId: 'my-execution-id',
+      result: '{ "data": 42 }',
+      error: null,
+    });
 }
 
 /**
@@ -187,11 +195,12 @@ function mockCall() {
  */
 function mockCallWithData() {
   return nock('https://cloudfunctions.googleapis.com')
-      .post(
-          '/v1/projects/el-gato/locations/us-central1/function/%F0%9F%A6%84:call')
-      .reply(200, {
-        executionId: 'my-execution-id',
-        result: '{ "data": 142 }',
-        error: null
-      });
+    .post(
+      '/v1/projects/el-gato/locations/us-central1/function/%F0%9F%A6%84:call'
+    )
+    .reply(200, {
+      executionId: 'my-execution-id',
+      result: '{ "data": 142 }',
+      error: null,
+    });
 }
